4ddf75b019e281bc20aa4ef33d68abed892fdcd6
Sushil Baid <sbaid@microsoft.com>
2016-09-25 12:31:19 +0530

    Hello world sample

---
+++ HelloWorld/Common/YearUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Common
+{
+    public class YearUtils
+    {
+        public static bool IsCurrentYear(DateTime date)
+        {
+            if (date.Year == DateTime.Now.Year)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ HelloWorld/MSTestTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckCurrentYear()
+        {
+            Assert.IsTrue(HelloWorld.Common.YearUtils.IsCurrentYear(new DateTime(2016, 1, 1)));
+        }
+
+        [TestMethod]
+        public void CheckLastYear()
+        {
+            Assert.IsFalse(HelloWorld.Common.YearUtils.IsCurrentYear(new DateTime(2015, 1, 1)));
+        }
+    }
+}
