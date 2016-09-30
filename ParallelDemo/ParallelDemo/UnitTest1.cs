946b592a3ae6a46e377e82b5a4f87cdde3aab676
Sushil Baid <sbaid@microsoft.com>
2016-09-30 11:52:15 +0530

    parallel & fakes sample

---
+++ ParallelDemo/ParallelDemo/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParallelDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod11()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod14()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod15()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod16()
+        {
+            ClassLibrary1.Class1.Sleep();
+        }
+    }
+}
---
+++ fakes/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IStockFeed feed = new ClassLibrary1.Fakes.StubIStockFeed()
+            {
+                GetStockPriceString = (code) => { return 100; }
+            };
+            var price = new StockAnalyzer().GetMsftStockPrice(feed);
+            Assert.AreEqual(100, price, "price is expected 100");
+        }
+        
+    }
+}
