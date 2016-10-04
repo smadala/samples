20a2927f2a29d2f0896dc16fa9f5f65c8d127c1f
Sushil Baid <sbaid@microsoft.com>
2016-10-04 14:53:13 +0530

    hello world mstestv1 test project

---
+++ HelloWorld-mstestv1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld_mstestv1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("TestMethod1 - Hello");
+        }
+    }
+}
