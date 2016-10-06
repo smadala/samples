5c65eac54719ab70e499ce86d411cd6378032b19
Sushil Baid <sbaid@microsoft.com>
2016-10-05 21:48:30 -0700

    sample to test solution with large number of tests

---
+++ ScaleTest-mstestv2/ScaleTest-mstestv2/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodWithReferences()
+        {
+            new ClassLibrary1.Class1().foo();
+            new ClassLibrary2.Class1().bar();
+            new ClassLibrary3.Class1().onemorebar();
+        }
+    }
+}
