e3f92d3b56aabf0891b943769d8b3a58cca10a3d
Sushil baid <sbaid@microsoft.com>
2016-10-14 02:55:46 -0700

    dev 14 - hello world UWP test

---
+++ d14/HelloWorld-uwp/HelloWorld-uwp/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace HelloWorld_uwp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
