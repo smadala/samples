0ebc7db5319a0bcd0d6075b46a0024f722bc2372
Sushil baid <sbaid@microsoft.com>
2016-10-14 04:29:02 -0700

    dev14 fakes sample

---
+++ d14/fakes/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.QualityTools.Testing.Fakes;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var msg = "hi there";
+            var interface1 = new ClassLibrary1.Fakes.StubInterface1()
+            {
+                SayHiString = (lang) =>
+                {
+                    return msg;
+                }
+            };
+            Assert.AreEqual(msg, ClassLibrary1.Class1.SayHiInEnglish(interface1));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            using (ShimsContext.Create())
+            {
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2000, 1, 1);
+                Assert.IsTrue(ClassLibrary1.Class1.IsCurrentYearLeapYear());
+            }
+        }
+    }
+}
---
+++ d14/fakes/fakes/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static string SayHiInEnglish(Interface1 call)
+        {
+            return call.SayHi("en");
+        }
+
+        public static bool IsCurrentYearLeapYear()
+        {
+            return DateTime.Now.Year % 4 == 0;
+        }
+    }
+}
