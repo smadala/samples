0aac18960bfa5c435a9aa70a0be88d9ca91717ca
Sushil Baid <sbaid@microsoft.com>
2016-11-05 21:02:13 -0700

    Adding Pex sample

---
+++ PexSample/ClassLibrary1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 4 == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
