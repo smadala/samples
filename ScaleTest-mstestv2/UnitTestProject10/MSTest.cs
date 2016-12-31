bd30e9e140b9be78adec1af3d774fd5c094a1ea8
Sushil Baid <sbaid@microsoft.com>
2016-12-31 12:02:29 +0530

    scale test update

---
+++ ScaleTest-mstestv2/UnitTestProject10/MSTest.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestinProgress
+{
+    [TestClass]
+	public class MSTestClass2
+    {
+
+		#region PassingTests
+		[TestMethod]
+		public void MSTest_P2_01()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_02()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_03()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_04()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_05()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_06()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_07()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_08()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_09()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_10()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_11()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_12()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_13()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_14()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_15()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_16()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_17()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_18()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_19()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_20()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_21()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_22()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_23()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_24()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_25()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_26()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_27()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_28()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_29()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_30()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_31()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_32()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_33()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_34()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_35()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_36()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_37()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_38()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_39()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_40()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_41()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_42()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_43()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_44()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_45()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_46()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_47()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_48()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_49()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		[TestMethod]
+		public void MSTest_P2_50()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.AreEqual(1, 1);
+		}
+		#endregion
+
+		#region FailingTests
+		#endregion
+    }
+}
