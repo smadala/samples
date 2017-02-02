43c0f501f8a0740a61ec6543c04ec170d21fa005
Sushil Baid <sbaid@microsoft.com>
2017-02-02 15:34:04 +0530

    traits sample

---
+++ TraitsSample/TraitsSample/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TraitsSample
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Owner("sbaid")]
+        [TestCategory("bvt")]
+        [TestProperty("k1", "v1")]
+        [TestProperty("k1", "v2")]
+        [Priority(1)]
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestProperty("k1", "v1")]
+        [Priority(1)]
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestCategory("bvt")]
+        [Priority(2)]
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [Owner("navb")]
+        [Priority(2)]
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+    }
+
+    [TestCategory("Sample")]
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod21()
+        {
+        }
+    }
+}
