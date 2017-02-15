b64fea23c143a902ea4550d5ec6a1833d098e375
Sushil Baid <sbaid@microsoft.com>
2017-02-15 12:16:01 +0530

    netcore sample update

---
+++ ScaleTest-netcore-mstestv2/UnitTestProject11/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject11
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
