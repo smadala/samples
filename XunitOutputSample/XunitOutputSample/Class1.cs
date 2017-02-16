d47cf6924ea0950abdbd77e5faf2be980ef7c802
Sushil Baid <sbaid@microsoft.com>
2017-02-16 09:50:01 +0530

    Xunit output sample

---
+++ XunitOutputSample/XunitOutputSample/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XunitOutputSample
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            this.foo();
+        }
+
+        private void foo()
+        {
+            this.bar();
+        }
+
+        private void bar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
