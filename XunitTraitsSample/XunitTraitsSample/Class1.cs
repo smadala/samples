779a1329dda1f83dfed10c4b878c14d3ebb83211
Sushil Baid <sbaid@microsoft.com>
2017-02-24 13:46:13 +0530

    Xunit traits sample

---
+++ XunitTraitsSample/XunitTraitsSample/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XunitTraitsSample
+{
+    public class TestClass1
+    {
+        [Fact]
+        public void foo()
+        {
+
+        }
+
+        [Fact]
+        public void bar()
+        {
+
+        }
+    }
+
+    public class TestClass2
+    {
+        [Trait("Category", "bvt")]
+        [Trait("Priority", "1")]
+        [Fact]
+        public void foo()
+        {
+
+        }
+       
+
+        [Fact]
+        public void bar()
+        {
+
+        }
+    }
+
+    public class TestClass3
+    {
+        [Trait("Category", "bvt")]
+        [Trait("Priority", "1")]
+        [Fact]
+        public void foo()
+        {
+
+        }
+
+        [Trait("Priority", "2")]
+        [Fact]
+        public void bar()
+        {
+        }
+    }
+}
