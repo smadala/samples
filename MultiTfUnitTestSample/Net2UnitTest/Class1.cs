c7722575e7f5472501ffca4913b395c44405d2bb
Sushil Baid <sbaid@microsoft.com>
2017-02-26 16:45:01 +0530

    Added Net2 andNet35

---
+++ MultiTfUnitTestSample/Net2UnitTest/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Net2UnitTest
+{
+    [TestClass]
+    public class Net2UnitTest
+    {
+        [TestMethod]
+        public void Foo()
+        {
+
+        }
+    }
+}
