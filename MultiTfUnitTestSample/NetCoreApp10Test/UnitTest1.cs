4d3e49b6b79db6d072f142ad073c75f3aa198e3c
Sushil Baid <sbaid@microsoft.com>
2017-03-02 16:44:07 +0530

    added netcoreapp1.0 est into multitf sample

---
+++ MultiTfUnitTestSample/NetCoreApp10Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetCoreApp10Test
+{
+    [TestClass]
+    public class NetCoreApp10UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
