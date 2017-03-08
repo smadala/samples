5b9236f7533d4b73422c386feeb07a2ca017e7d7
Sushil Baid <sbaid@microsoft.com>
2017-03-08 22:09:18 +0530

    net core 10 console app sample

---
+++ NetCoreConsoleApp10/NetCoreConsoleApp10/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NetCoreConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ClassLibrary1.Class1().foo();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
