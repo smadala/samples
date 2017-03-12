5ea5b021be3eac64541223d7a95b6c853f7ae754
Sushil Baid <sbaid@microsoft.com>
2017-03-12 10:35:03 +0530

    net core ut sample

---
+++ HelloWorld-NetCore/HelloWorld-NetCore/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld_NetCore
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
