60027d29a3b828bf2bcdc59daa078d7c3a2d4970
Sushil Baid <sbaid@microsoft.com>
2017-04-25 14:02:09 +0530

    updated to netcoreapp1.1

---
+++ XUnitSample/XUnitSample/XunitTest.cs
@@ -0,0 +1,915 @@
+﻿using System;
+using Xunit;
+
+namespace UnitTestinProgress
+{
+    public class MSTestClass1
+    {
+
+		#region PassingTests
+		[Fact]
+		public void MSTest_P1_01()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_02()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_03()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_04()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_05()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_06()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_07()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_08()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_09()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_10()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_11()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_12()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_13()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_14()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_15()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_16()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_17()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_18()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_19()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_20()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_21()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_22()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_23()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_24()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_25()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_26()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_27()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_28()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_29()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_30()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_31()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_32()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_33()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_34()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_35()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_36()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_37()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_38()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_39()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_40()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_41()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_42()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_43()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_44()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_45()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_46()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_47()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_48()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_49()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_50()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_51()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_52()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_53()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_54()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_55()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_56()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_57()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_58()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_59()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_60()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_61()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_62()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_63()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_64()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_65()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_66()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_67()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_68()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_69()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_70()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_71()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_72()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_73()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_74()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_75()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_76()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_77()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_78()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_79()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_80()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_81()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_82()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_83()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_84()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_85()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_86()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_87()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_88()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_89()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_90()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_91()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_92()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_93()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_94()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_95()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_96()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_97()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_98()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_99()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_100()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_101()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_102()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_103()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_104()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_105()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_106()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_107()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_108()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_109()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_110()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_111()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_112()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_113()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_114()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_115()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_116()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_117()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_118()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_119()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_120()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_121()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_122()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_123()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_124()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_125()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_126()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_127()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_128()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_129()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_130()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_131()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_132()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_133()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_134()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_135()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_136()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_137()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_138()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_139()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_140()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_141()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_142()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_143()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_144()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_145()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_146()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_147()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_148()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_149()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		[Fact]
+		public void MSTest_P1_150()
+		{
+			//System.Threading.Thread.Sleep(1500);
+			Assert.Equal(1, 1);
+		}
+		#endregion
+
+		#region FailingTests
+		#endregion
+    }
+}
