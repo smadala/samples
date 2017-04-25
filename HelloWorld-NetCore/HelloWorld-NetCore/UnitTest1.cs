55eaf2b3a1f1af3d42f6b26226236b014657393f
Sushil Baid <sbaid@microsoft.com>
2017-04-25 14:27:11 +0530

    Updating to package reference 1.1.17 and using Console.WriteLines

--- HelloWorld-NetCore/HelloWorld-NetCore/UnitTest1.cs
+++ HelloWorld-NetCore/HelloWorld-NetCore/UnitTest1.cs
@@ -9,6 +9,14 @@ namespace HelloWorld_NetCore
         [TestMethod]
         public void TestMethod1()
         {
+            new ClassLibrary1.Class1().Foo();
+            Console.WriteLine("hi there");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Console.WriteLine("hi there - TestMethod2");
         }
     }
 }
