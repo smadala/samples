74c7eaeadcdfeef19f9235037ed1feace5e1c54c
Sushil Baid <sbaid@microsoft.com>
2017-04-28 09:59:07 +0530

    test crash repro sample

---
+++ NetCoreCrashTest/NetCoreCrashTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Xunit;
+
+namespace NetCoreCrashTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Fails()
+        {
+            Action fail = () => Debug.Assert(false, "Oops");
+            var thread = new Thread(new ThreadStart(fail));
+            thread.Start();
+            thread.Join();
+        }
+    }
+}
