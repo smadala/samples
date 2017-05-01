d9f6762c0755c98c80d55b12b0d8f9e6341a6f74
Sushil Baid <sbaid@microsoft.com>
2017-05-01 11:56:35 +0530

    Add TestMethod in nested class

---
+++ HelloWorld/MSTestTests/UnitTest2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void PlaceHolderTest()
+        {
+        }
+
+        [TestClass]
+        public class NestedClass
+        {
+            [TestMethod]
+            public void NestedTestMethod()
+            {
+
+            }
+        }
+    }
+}
