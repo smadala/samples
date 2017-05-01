2c0bd0bf06aaa65e6dc42a417e696918d5487af6
Sushil Baid <sbaid@microsoft.com>
2017-05-01 11:54:05 +0530

    Added test for a bug

---
+++ HelloWorld/MSTestTests/MoreTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject3
+{
+    [TestClass]
+    public class MoreTests
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void AnotherInconclusiveTest()
+        {
+        }
+    }
+}
