0f39c951d8c1397a6795ba395eccd8cd30290876
Sushil Baid <sbaid@microsoft.com>
2017-05-05 09:01:30 +0530

    nunit sample

---
+++ HelloWorld-NUnit/HelloWorld-NUnit/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_NUnit
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
