1d1f4253f96509e5efb938592ebdce1961b0bbaa
Satya Madala <samadala@microsoft.com>
2018-02-05 20:27:09 +0530

    Add comImportIssue repro

---
+++ ComImportIssue/consoleapp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using nslib;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 class1= new Class1();
+            Test1();
+        }
+
+        public static void Test1()
+        {
+            foreach( var t in  GetAllTypesOf<ClassFactory>()){
+                System.Console.WriteLine(t.ToString());
+            }
+        }
+
+        private static IEnumerable<Type> GetAllTypesOf<T>()
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.DefinedTypes)
+                .Where(type => typeof(T).GetTypeInfo().IsAssignableFrom(type.AsType()))
+                .Select(type => type.AsType());
+        }
+    }
+}
---
+++ ComImportIssue/nslib/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace nslib
+{
+    public class Class1
+    {
+        
+        
+    }
+    [ComImport, ComVisible(false), Guid("00000001-0000-0000-C000-000000000046"),  InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IClassFactory
+        {
+            void CreateInstance(
+                [MarshalAs(UnmanagedType.Interface)] object aggregator,
+                ref Guid refiid,
+                [MarshalAs(UnmanagedType.Interface)] out object createdObject);
+
+            void LockServer(bool incrementRefCount);
+        }
+        [ComImport, Guid("00000001-0000-0000-C000-000000000046")]
+    public class ClassFactory{
+
+        }
+}
