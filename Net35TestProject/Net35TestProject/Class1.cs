763df23c3808579d90a5e75d6e9f3ea7fbf07f3e
Satya Madala <samadala@microsoft.com>
2018-08-03 11:20:01 +0530

    Add Net 35 project

---
+++ Net35TestProject/Net35TestProject/Class1.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Net35TestProject
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TestClass1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
