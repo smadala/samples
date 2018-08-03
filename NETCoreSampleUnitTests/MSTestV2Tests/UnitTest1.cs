d394144ab6efa4dc905e27c723cc89340811899f
Satya Madala <samadala@microsoft.com>
2018-08-03 11:26:34 +0530

    NET core sample test project

---
+++ NETCoreSampleUnitTests/MSTestV2Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestV2Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MSTestTestMethod1()
+        {
+        }
+    }
+}
