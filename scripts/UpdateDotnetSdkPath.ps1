bc4ca4ffbfbfa0c5fb07107305d26e9fb0ce41f6
Satya Madala <samadala@microsoft.com>
2018-08-03 14:38:04 +0530

    Add test command

--- scripts/UpdateDotnetSdkPath.ps1
+++ scripts/UpdateDotnetSdkPath.ps1
@@ -5,4 +5,4 @@ $pathValue = $path.Value -Replace "C:\\Program Files\\dotnet","C:\dotnet";
 Write-Host $pathValue; 
 $env:Path = $pathValue; 
 dotnet --version
-dotnet NETCoreSampleUnitTests\MSTestV2Tests\MSTestV2Tests.csproj --logger trx --collect "Code coverage"
+dotnet test NETCoreSampleUnitTests\MSTestV2Tests\MSTestV2Tests.csproj --logger trx --collect "Code coverage"
